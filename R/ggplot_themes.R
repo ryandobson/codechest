8763dd46f170013e3ce83a15e0eda1ee78ae594c
ryandobson <rdobson17@gmail.com>
2025-09-16 07:55:17 -0500

    Small updates to ggplot theme -- strip.background fill and adding in base_family text

--- R/ggplot_themes.R
+++ R/ggplot_themes.R
@@ -24,28 +24,33 @@ utils::globalVariables(c("%+replace%", "element_blank", "element_line",
 jermeys_theme <- function(base_size = 24, base_family = "") {
   ggplot2::theme_grey(base_size = base_size, base_family = base_family) + #%+replace%
     ggplot2::theme(
+
       axis.line = ggplot2::element_blank(),
-      axis.text.x = ggplot2::element_text(size = base_size * 0.8, color = "black", lineheight = 0.9),
+      axis.text.x = ggplot2::element_text(size = base_size * 0.8, color = "black", lineheight = 0.9, family = base_family),
       axis.text.y = ggplot2::element_text(size = base_size * 0.8, color = "black", lineheight = 0.9),
       axis.ticks = ggplot2::element_line(color = "black", size = 0.2),
-      axis.title.x = ggplot2::element_text(size = base_size, color = "black", margin = ggplot2::margin(10, 0, 0, 0)),
-      axis.title.y = ggplot2::element_text(size = base_size, color = "black", angle = 90, margin = ggplot2::margin(0, 10, 0, 0)),
+      axis.title.x = ggplot2::element_text(size = base_size, color = "black", margin = ggplot2::margin(10, 0, 0, 0), family = base_family),
+      axis.title.y = ggplot2::element_text(size = base_size, color = "black", angle = 90, margin = ggplot2::margin(0, 10, 0, 0), family = base_family),
       axis.ticks.length = ggplot2::unit(0.3, "lines"),
+
       legend.background = ggplot2::element_rect(color = NA, fill = "#ffffff"),
       legend.key = ggplot2::element_rect(color = "black", fill = "#ffffff"),
       legend.key.size = ggplot2::unit(2, "lines"),
-      legend.text = ggplot2::element_text(size = base_size * 0.8, color = "black"),
-      legend.title = ggplot2::element_text(size = base_size * 0.8, face = "bold", color = "black"),
+      legend.text = ggplot2::element_text(size = base_size * 0.8, color = "black", family = base_family),
+      legend.title = ggplot2::element_text(size = base_size * 0.8, face = "bold", color = "black", family = base_family),
+
       panel.background = ggplot2::element_rect(fill = "#ffffff", color = NA),
       panel.border = ggplot2::element_rect(fill = NA, color = "black"),
       panel.grid.major = ggplot2::element_line(color = "#ffffff"),
       panel.grid.minor = ggplot2::element_line(color = "#ffffff"),
       panel.spacing = ggplot2::unit(2, "lines"),
-      strip.background = ggplot2::element_rect(fill = "grey30", color = "grey10"),
-      strip.text.x = ggplot2::element_text(size = base_size * 0.8, color = "black"),
-      strip.text.y = ggplot2::element_text(size = base_size * 0.8, color = "black", angle = -90),
+
+
+      strip.background = ggplot2::element_rect(fill = "#ffffff", color = "grey10"),
+      strip.text.x = ggplot2::element_text(size = base_size * 0.8, color = "black", family = base_family),
+      strip.text.y = ggplot2::element_text(size = base_size * 0.8, color = "black", angle = -90, family = base_family),
       plot.background = ggplot2::element_rect(color = "#ffffff", fill = "#ffffff"),
-      plot.title = ggplot2::element_text(size = base_size * 1.2, color = "black"),
+      plot.title = ggplot2::element_text(size = base_size * 1.2, color = "black", family = base_family),
       plot.margin = ggplot2::unit(rep(1, 4), "lines")
     )
 }
